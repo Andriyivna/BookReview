cf594af1a92ed9c7a9fe1f327cad1e932c82f389
thestahan <thestahan@gmail.com>
2021-03-22 12:35:43 +0100

    Added basic entites

---
+++ API/Entities/Author.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace API.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
