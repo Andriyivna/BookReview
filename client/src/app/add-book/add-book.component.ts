bdd17377e190981cb64b7d6a0817318dd85a4ebe
hax33l <80815886+hax33l@users.noreply.github.com>
2021-05-21 13:57:01 +0200

    #52
    add-book coverImg

--- client/src/app/add-book/add-book.component.ts
+++ client/src/app/add-book/add-book.component.ts
@@ -4,6 +4,15 @@ import { Router } from '@angular/router';
 import { Author } from '../bookcase/components/authors/authors.component';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+
+export interface FileToUpload {
+  fileName: string;
+  fileSize: number;
+  fileType: string;
+  lastModifiedTime: number;
+  lastModifiedDate: Date;
+  fileAsBase64: string;
+}
 export interface Book {
   id?: number;
   title: string;
@@ -43,37 +52,37 @@ export class AddBookComponent implements OnInit {
   nrSelectAuth = 1;
   nrSelectGen = 1;
 
-  alert: Alert = {type: 'success', message: 'A new book has been added!'}
+  alert: Alert = { type: 'success', message: 'A new book has been added!' }
 
-  constructor(private http:HttpClient,private router: Router, private fb: FormBuilder) { }
+  constructor(private http: HttpClient, private router: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    if(localStorage.getItem('token') != null){
+    if (localStorage.getItem('token') != null) {
       this.isLoggedIn = true;
-    }else{
+    } else {
       this.router.navigate(['/login']);
     }
     this.addBookForm = this.fb.group({
       id: 0,
-      title: ['',[
+      title: ['', [
         Validators.required
       ]],
-      publisher: ['',[
+      publisher: ['', [
         Validators.required
       ]],
-      releaseYear: ['',[
+      releaseYear: ['', [
         Validators.required
       ]],
-      authorId: [,[
+      authorId: [, [
         Validators.required
       ]],
-      genreId: [,[
+      genreId: [, [
         Validators.required
       ]],
-      coverImg: ['',[
+      coverImg: ['', [
         Validators.required
       ]],
-      description: ['',[
+      description: ['', [
         Validators.required
       ]],
       averageRates: 0
@@ -88,56 +97,104 @@ export class AddBookComponent implements OnInit {
         this.authors = data;
       });
     this.getGenres()
-      .then((data)=> {
+      .then((data) => {
         this.genres = data;
       });
   }
   str: any;
   bookId: any;
-  addBook(){
-    var genre:any;
-    var formValue:any;
+  addBook() {
+    var genre: any;
+    var formValue: any;
 
-    this.str = this.addBookForm.get('coverImg').value.split( '\\' );
+    this.str = this.addBookForm.get('coverImg').value.split('\\');
     formValue = this.addBookForm.value;
-    formValue['coverImg'] = this.str[this.str.length-1];
-    
-    for(var i = 0; i < this.genres.length; i++){
-      if(this.genres[i].name == formValue['genreId']){
+    formValue['coverImg'] = this.str[this.str.length - 1];
+
+    for (var i = 0; i < this.genres.length; i++) {
+      if (this.genres[i].name == formValue['genreId']) {
         formValue['genreId'] = this.genres[i].id;
         break;
       }
     }
-    for(var i = 0; i < this.authors.length; i++){
-      if(this.authors[i].firstName + ' ' +this.authors[i].secondName == formValue['authorId']){
+    for (var i = 0; i < this.authors.length; i++) {
+      if (this.authors[i].firstName + ' ' + this.authors[i].secondName == formValue['authorId']) {
         formValue['authorId'] = this.authors[i].id;
         break;
       }
     }
-    return this.http.post(this.ApiURL+'/Book/add',formValue);
+    return this.http.post(this.ApiURL + '/Book/add', formValue);
   }
   fetchedBook: Book;
+
   onSubmit() {
-    this.addBook().subscribe(
-      (res:any) =>{
-        this.isSent = true;
-        this.getBook(this.addBookForm.get('title').value).then(data => {
-          this.router.navigateByUrl('/book/'+data.id)
-        })
-      },
-      err => {
-        console.log(err);
-      }
-    )
+    this.readAndUploadFile(this.addBookForm.get('coverImg'));
   }
 
-  getBook(title:string): Promise<Book>{
-    return this.http.get<Book>(this.ApiURL+'/Book/title/'+title).toPromise();
+  getBook(title: string): Promise<Book> {
+    return this.http.get<Book>(this.ApiURL + '/Book/title/' + title).toPromise();
   }
-  getAll(): Promise<Array<Author>>{
-    return this.http.get<Array<Author>>(this.ApiURL+'/Authors/').toPromise();
+  getAll(): Promise<Array<Author>> {
+    return this.http.get<Array<Author>>(this.ApiURL + '/Authors/').toPromise();
   }
-  getGenres(): Promise<Array<Genre>>{
-    return this.http.get<Array<Genre>>(this.ApiURL+'/Genres').toPromise();
+  getGenres(): Promise<Array<Genre>> {
+    return this.http.get<Array<Genre>>(this.ApiURL + '/Genres').toPromise();
+  }
+
+  theFile: any;
+  onFileChange(event: any) {
+    const MAX_SIZE: number = 4194304;
+    this.theFile = null;
+    if (event.target.files && event.target.files.length > 0) {
+      if (event.target.files[0].size < MAX_SIZE) {
+        console.log(event.target.files[0]);
+        this.theFile = event.target.files[0];
+        let reader = new FileReader();
+        reader.readAsDataURL(event.target.files[0]);
+        reader.onload = (event => {
+          //this.imageSrc = reader.result;
+        })
+      }
+      else {
+        // Display error message
+        //this.messages.push("File: " + event.target.files[0].name + " is too large to upload.");
+      }
+    }
+  }
+
+  private readAndUploadFile(theFile: any) {
+    let file: FileToUpload;
+    // Set File Information
+    console.log(theFile)
+    file.fileName = theFile.name;
+    file.fileSize = theFile.size;
+    file.fileType = theFile.type;
+    file.lastModifiedTime = theFile.lastModified;
+    file.lastModifiedDate = theFile.lastModifiedDate;
+    let reader = new FileReader();
+    // Setup onload event for reader
+    reader.onload = async () => {
+      // Store base64 encoded representation of file
+      file.fileAsBase64 = reader.result.toString();
+      // POST to server
+      this.http.post(this.ApiURL + '/Book/addPhoto', file, { responseType: 'text' }).subscribe(resp => {
+        this.addBookForm.get('coverImg').value(resp);
+        console.log(this.addBookForm.get('coverImg').value);
+        this.addBook().subscribe(
+          (res: any) => {
+            this.isSent = true;
+            this.getBook(this.addBookForm.get('title').value).then(data => {
+              this.router.navigateByUrl('/book/' + data.id)
+            })
+          },
+          err => {
+            console.log(err);
+          }
+        )
+      });
+    }
+    // Read the file
+    reader.readAsDataURL(theFile);
+    console.log("end readAndUploadFile")
   }
 }
